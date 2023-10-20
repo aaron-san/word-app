5a9a6ff06bad9016d1865c4b7b522d2159eaace7
fincoder <aaronhardy6@gmail.com>
2023-10-20 16:43:28 +0900

    update config for github pages

--- README.md
+++ README.md
@@ -14,9 +14,10 @@ This project uses the json-server library for the backend.
 
 ```bash
 git init
+# git add <file>
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/aaron-san/English-Dictionary-App.git
+git remote add origin https://github.com/aaron-san/english-dictionary-app.git
 git add README.md
 git push -u origin main
 ```
@@ -25,4 +26,21 @@ git push -u origin main
 
 ```bash
 git remote remove origin
+```
+
+## Add GitHub Pages Dependency App
+
+```bash
+npm install gh-pages --save-dev
+```
+
+### package.json:
+
+```javascript
+"scripts": {
+"server": "concurrently \"json-server --watch db.json\" \"node ./export-xl.cjs\"",
+"dev": "concurrently \"npm run server\" \"vite\"",
+"predeploy": "tsc && vite build",
+"deploy": "gh-pages -d dist"
+},
 ```
--- src/context/wordContext.tsx
+++ src/context/wordContext.tsx
@@ -1,36 +1,31 @@
 import React, { createContext, useState } from "react";
 
-
 interface IWord {
-    id: number;
-    word: string;
-    meaning: string;
-  }
-
-  type IWordsContextType = {
-    words: IWord[];
-  }
-
-const WordProvider: React.FC<React.ReactNode> = ({children}) => {
-    const [words, setWords] = useState<IWord[]>([
-        id: 1,
-        word: "sophisticated",
-        meaning: "intelligent, consisting of complex matter",
-    ])
-    
-
+  id: number;
+  word: string;
+  meaning: string;
 }
 
-
-export const MyContext = createContext<IWordsContextType | null>(null);
-
-export const ContextProvider = ({ children: any }) => {
-
-    
-
-  return (
-    <MyContext.Provider value={[books, setbooks]}>
-      {children}
-    </MyContext.Provider>
-  );
+type IWordsContextType = {
+  words: IWord[];
 };
+
+// const WordProvider: React.FC<React.ReactNode> = ({ children }) => {
+//   const [words, setWords] = useState<IWord[]>([
+//     {
+//       id: 1,
+//       word: "sophisticated",
+//       meaning: "intelligent, consisting of complex matter",
+//     },
+//   ]);
+// };
+
+// export const MyContext = createContext<IWordsContextType | null>(null);
+
+// export const ContextProvider = ({ children: any }) => {
+//   return (
+//     <MyContext.Provider value={[books, setbooks]}>
+//       {children}
+//     </MyContext.Provider>
+//   );
+// };
